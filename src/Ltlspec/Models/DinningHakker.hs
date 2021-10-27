d0dc348286fd33212fbe2f6f14f762778d3f7ff6
funemy <liyzunique@gmail.com>
2021-10-27 15:25:38 -0700

    some typeclass def

--- src/Ltlspec/Models/DinningHakker.hs
+++ src/Ltlspec/Models/DinningHakker.hs
@@ -23,21 +23,28 @@ data ChopstickMsg =
       Grant ChopstickId HakkerId
     | Busy ChopstickId HakkerId
 
+class Msg m
+
+instance Msg HakkerMsg
+instance Msg ChopstickMsg
+
 data Hakker = Hakker
                 { hid :: HakkerId
+                -- in-transit messages sent by Hakker
                 , hkMsgs :: [HakkerMsg]
                 , hkState :: HakkerState
-                , lchop :: Int
-                , rchop :: Int
+                , lchop :: ChopstickId
+                , rchop :: ChopstickId
                 }
 
 data Chopstick = Chopstick
                    { cid :: ChopstickId
+                   -- in-transit messages sent by Chopstick
                    , chopMsgs :: [ChopstickMsg]
                    }
 
-type Hakkers = M.Map String Hakker
-type Chopsticks = M.Map Int Chopstick
+type Hakkers = M.Map HakkerId Hakker
+type Chopsticks = M.Map ChopstickId Chopstick
 type Traces = [Trace]
 
 data GlobalState = GlobalState Hakkers Chopsticks
@@ -45,8 +52,16 @@ data Trace = Trace TimeStamp (Either HakkerMsg ChopstickMsg) GlobalState
 
 -- Is this needed?
 class Actor a where
-    send :: a -> a
-    recv :: a -> a
+    send :: Actor to => Msg msg => a -> to -> msg -> GlobalState
+    recv :: Actor from => Msg msg => from -> a -> msg -> GlobalState
+
+instance Actor Hakker where
+    send to from msg = undefined
+    recv to from msg = undefined
+
+instance Actor Chopstick where
+    send to from msg = undefined
+    recv to from msg = undefined
 
 -- data World = ?
 
