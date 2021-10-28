536179b43bb614a11637f2be12ea8cd50182d4bd
funemy <liyzunique@gmail.com>
2021-10-27 22:55:23 -0700

    init state

--- src/Ltlspec/Models/DinningHakker.hs
+++ src/Ltlspec/Models/DinningHakker.hs
@@ -1,6 +1,6 @@
 module Ltlspec.Models.DinningHakker where
 
-import Data.Map as M
+import qualified Data.Map as M
 
 type TimeStamp = Int
 type HakkerId = String
@@ -50,18 +50,29 @@ type Traces = [Trace]
 data GlobalState = GlobalState Hakkers Chopsticks
 data Trace = Trace TimeStamp (Either HakkerMsg ChopstickMsg) GlobalState
 
--- Is this needed?
 class Actor a where
-    send :: Actor to => Msg msg => a -> to -> msg -> GlobalState
-    recv :: Actor from => Msg msg => from -> a -> msg -> GlobalState
+    -- send :: Actor to => Msg msg => a -> to -> msg -> GlobalState
+    -- recv :: Actor from => Msg msg => from -> a -> msg -> GlobalState
 
 instance Actor Hakker where
-    send to from msg = undefined
-    recv to from msg = undefined
+    -- send to from msg = undefined
+    -- recv to from msg = undefined
 
 instance Actor Chopstick where
-    send to from msg = undefined
-    recv to from msg = undefined
+    -- send to from msg = undefined
+    -- recv to from msg = undefined
+
+initState :: [HakkerId] -> GlobalState
+initState hs = GlobalState hakkers chopsticks
+    where
+        l = length hs
+        idx = init $ scanl (\c _ -> c + 1) 0 hs
+        chopsticks = foldl (\m i -> M.insert i Chopstick{cid=i, chopMsgs=[]} m) M.empty idx
+        hakkers = foldl (\m i -> M.insert (hs !! i) Hakker{hid=(hs !! i), hkMsgs=[], hkState=Thinking, lchop=i, rchop=(i-1) `mod` l} m) M.empty idx
+
+
+step :: GlobalState -> GlobalState
+step = undefined
 
 -- data World = ?
 
