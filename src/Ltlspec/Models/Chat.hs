e246fafabb1d4ebf9e15aa95bc6133aca8847da2
Eric Conlon <ejconlon@gmail.com>
2021-10-28 08:54:48 -0700

    intro aeson

--- src/Ltlspec/Models/Chat.hs
+++ src/Ltlspec/Models/Chat.hs
@@ -1,12 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Ltlspec.Models.Chat where
 
+import Data.Aeson (ToJSON (..), (.=), object)
 import System.Random (mkStdGen, randomR, StdGen)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 
 type ActionID = Integer
 
-type ClientID = Integer 
+type ClientID = Integer
 
 type ChannelName = Integer
 
@@ -14,17 +17,22 @@ type MessageContent = String
 
 data ClientAction =
     List ActionID ClientID
-    | Join ActionID ClientID ChannelName
-    | Leave ActionID ClientID ChannelName
-    | Send ActionID ClientID MessageContent ChannelName
-    deriving (Eq, Show, Ord)
+  | Join ActionID ClientID ChannelName
+  | Leave ActionID ClientID ChannelName
+  | Send ActionID ClientID MessageContent ChannelName
+  deriving stock (Eq, Show, Ord)
+
+instance ToJSON ClientAction where
+  toJSON = \case
+    List aid cid -> object ["type" .= ("list" :: String), "action" .= aid, "client" .= cid]
+    _ -> undefined
 
 data ServerResponse =
     Share ActionID MessageContent ClientID
-    | NewJoin ActionID ClientID ChannelName ClientID
-    | NewLeave ActionID ClientID ChannelName ClientID
-    | ChannelList ActionID ClientID ChannelName
-    deriving (Eq, Show, Ord)
+  | NewJoin ActionID ClientID ChannelName ClientID
+  | NewLeave ActionID ClientID ChannelName ClientID
+  | ChannelList ActionID ClientID ChannelName
+  deriving stock (Eq, Show, Ord)
 
 type SystemEvent = Either ClientAction ServerResponse
 
@@ -74,35 +82,35 @@ getRandomElementOfList :: [a] -> StdGen -> a
 getRandomElementOfList l gen = let randomIndex = fst (randomR (0, ( (length l) - 1 )) gen) in l !! randomIndex
 
 simulationStep :: (SystemState, StdGen) -> (SystemState, StdGen)
-simulationStep ((state, trace, seed), gen) = 
-    let 
-        randomActionIndex = fst (randomR (0::Int, 3) gen) 
-        newgen = snd (randomR (0::Int, 3) gen) 
+simulationStep ((state, trace, seed), gen) =
+    let
+        randomActionIndex = fst (randomR (0::Int, 3) gen)
+        newgen = snd (randomR (0::Int, 3) gen)
     in
     let actionname = ["send", "join", "leave", "list"] !! randomActionIndex in
     case actionname of
-        "send" -> let 
+        "send" -> let
                     client = getRandomElementOfList (Map.keys state) gen
-                    channel = getRandomElementOfList (Map.findWithDefault [] client state) gen 
+                    channel = getRandomElementOfList (Map.findWithDefault [] client state) gen
                     message = "FOO"
-                  in if null (Map.findWithDefault [] client state) then 
+                  in if null (Map.findWithDefault [] client state) then
                         ((state, trace, seed), gen)
-                    else 
+                    else
                         (send (state, trace, seed) client message channel, newgen)
-        "join" -> let 
-                    client = getRandomElementOfList (Map.keys state) gen 
+        "join" -> let
+                    client = getRandomElementOfList (Map.keys state) gen
                     allchanels = getJoinableChannels state client
                     channel = getRandomElementOfList ( allchanels ++ [last allchanels + 1]) gen
                   in (join (state, trace, seed) client channel, newgen)
-        "leave" -> let 
-                        client = getRandomElementOfList (Map.keys state) gen 
-                        channel = getRandomElementOfList (Map.findWithDefault [] client state) gen 
-                    in if null (Map.findWithDefault [] client state) then 
+        "leave" -> let
+                        client = getRandomElementOfList (Map.keys state) gen
+                        channel = getRandomElementOfList (Map.findWithDefault [] client state) gen
+                    in if null (Map.findWithDefault [] client state) then
                         ((state, trace, seed), gen)
-                    else 
+                    else
                         (leave (state, trace, seed) client channel, newgen)
-        "list" ->  let 
-                        client = getRandomElementOfList (Map.keys state) gen 
+        "list" ->  let
+                        client = getRandomElementOfList (Map.keys state) gen
                     in (list (state, trace, seed) client, newgen)
 
 generator :: Integer -> Integer -> (SystemState, StdGen)
